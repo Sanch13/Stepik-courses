dc4052605c163f18ec9a668637d3ed48284ab5c8
sanch13 <sanch13sanch@gmail.com>
2023-12-26 20:39:44 +0300

    create proj, added app course, added logging

--- Backend_for_django_course/proj/course/mainapp/urls.py
+++ Backend_for_django_course/proj/course/mainapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('hello/', views.GreetingView.as_view(), name="hello")
 ]
--- Backend_for_django_course/proj/course/mainapp/views.py
+++ Backend_for_django_course/proj/course/mainapp/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
+from django.views import View
 from django.shortcuts import render
+
+
+class GreetingView(View):
+    def get(self, request):
+        return HttpResponse("Hello, World")
 
 
 def index(request):
