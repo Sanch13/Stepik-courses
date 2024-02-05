96e82ca752ea279877dabc91f0f136d479c239bc
sanch13 <sanch13sanch@gmail.com>
2024-02-05 08:00:26 +0300

    work with sql requests

--- Interactive SQL Simulator/querysets.py
+++ Interactive SQL Simulator/querysets.py
@@ -42,8 +42,8 @@
 
 # 1.2.8 Вывести название и авторов тех книг, цены которых принадлежат интервалу от 540.50 до 800
 # (включая границы),  а количество или 2, или 3, или 5, или 7 .
-query = """
-SELECT title, author
-FROM book
-WHERE (price BETWEEN 540.50 AND 800) AND (amount IN (2, 3, 5, 7));
-"""
+# query = """
+# SELECT title, author
+# FROM book
+# WHERE (price BETWEEN 540.50 AND 800) AND (amount IN (2, 3, 5, 7));
+# """
