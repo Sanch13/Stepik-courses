558b90040b5a290441494027cb912b115d97ffa1
sanch13 <sanch13sanch@gmail.com>
2024-02-05 20:23:01 +0300

    work with sql requests

--- Interactive SQL Simulator/manipulate_data.py
+++ Interactive SQL Simulator/manipulate_data.py
@@ -3,7 +3,7 @@ from pprint import pprint
 from mysql.connector import Error
 
 from conn_to_bd import connect_to_bd
-from querysets import query
+from querysets_1_3 import query
 
 
 def get_data_from_db(query):
